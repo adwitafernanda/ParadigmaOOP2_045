a05a1b07da0912ebfbccf6bf14886d8be9e1b31b
adwitafernanda <adwitafernanda4@gmail.com>
2023-05-31 17:51:43 +0700

    end

--- problemMultiWaris/problemMultiWaris.cpp
+++ problemMultiWaris/problemMultiWaris.cpp
@@ -27,3 +27,29 @@ public:
 		cout << "manusia dihapus\n" << endl;
 	}
 }; 
+
+class pelajar : private manusia, public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "HALLO, nama saya " + nama + "dengan jenis kelamin"
+			+ jenisKelamin + "dari sekolah" + sekolah + "\n\n";
+	}
+};
+
+int main() {
+	pelajar siswa("andi laksono", "laki-laki", "SMAN 1 Bantul");
+	cout << siswa.perkenalan();
+
+	return 0;
+}
